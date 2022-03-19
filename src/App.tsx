1140da9ec1fd6908cf98b0093f8c97c3bb44802f
xiayuushi <xiayuushi@qq.com>
2022-03-19 15:42:01 +0800

    项目初始化 - 配置基础路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import './App.scss'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import NotFound from './pages/NotFound'
+
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
-      App
+      <Router>
+        <Switch>
+          <Redirect exact from='/' to='/layout'></Redirect>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/layout' component={Layout}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
 
 export default App
+
+// 01、react-router-dom@5.3.0路由的三种使用方式
+// 01、方式1 `<Route path='/xxx' component={Xxx}></Route>`
+// 01、方式2 `<Route path='/xxx' render={()=>(<Xxx></Xxx>)}></Route>`
+// 01、方式3 `<Route path='/xxx'><Xxx></Xxx></Route>`
+
+// 02、react-router-dom@5.3.0路由重定向的两种使用方式
+// 02、方式1 `<Redirect exact from='/aaa' to='/xxx'></Redirect>`
+// 02、方式2 `<Route exact path='/aaa' render={()=>(<Redirect to='/xxx'></Redirect>)}></Route>`
+// N1、Redirect的from属性生效的前提是，该组件嵌套在Switch组件内
+// N1、Redirect组件中的from属性脱离Switch组件包裹后不会生效，此时相当于from='*'
