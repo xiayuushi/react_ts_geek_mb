b0db73143a2dcc5e2979e0d24a8c421c777e5fcf
xiayuushi <xiayuushi@qq.com>
2022-03-21 08:20:19 +0800

    项目初始化 - 配置px自动转vw

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const px2vw = require('postcss-px-to-viewport')
+
 module.exports = {
   webpack: {
     alias: {
@@ -10,6 +12,11 @@ module.exports = {
       '@utils': path.resolve(__dirname, 'src', 'utils'),
       '@assets': path.resolve(__dirname, 'src', 'assets'),
       '@components': path.resolve(__dirname, 'src', 'components'),
+    },
+  },
+  style: {
+    postcss: {
+      plugins: [px2vw({ viewportWidth: 375 })],
     },
   },
 }
@@ -23,3 +30,13 @@ module.exports = {
 
 // N1、项目配置文件更新后必须重启项目才会生效
 // N2、配置完别名后未配置vscode路径识别，重启项目后可能会报错找不到xxx模块，因此建议以上4步骤完成后再重启项目进行别名使用
+
+// 使用`postcss-px-to-viewport`实现移动端vw适配的流程
+// st1、安装`postcss-px-to-viewport`（指令：yarn add -D postcss-px-to-viewport）
+// st2、在craco.config.js中配置（因为react项目中配置webpack文件被隐藏，因此需要通过craco的配置文件去覆盖）
+// st2、const px2vw = require('postcss-px-to-viewport');
+// st2、module.exports = { style: { postcss: { plugins: [px2vw({viewportWidth: 375,})] }}}
+// st3、后续书写CSS时直接使用px单位，px单位经过编译后会，会被转成适应视口的vw
+
+// N1、viewportWidth用于指定设计稿的宽度
+// N1、此处设置的是一倍图375宽度，测量1px就写1px会被`postcss-px-to-viewport`自动在编译后转成vw单位
--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 const App = () => {
   return (
     <div className='app'>
+      <span>222</span>
       <Router>
         <Switch>
           <Redirect exact from='/' to='/layout'></Redirect>
