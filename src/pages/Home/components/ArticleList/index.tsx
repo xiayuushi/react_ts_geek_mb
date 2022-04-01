8f6a418fdfb0f44232e3ee1e5765fec98579b129
xiayuushi <xiayuushi@qq.com>
2022-04-01 16:16:27 +0800

    首页模块 - 文章列表上拉加载更多

--- src/pages/Home/components/ArticleList/index.tsx
+++ src/pages/Home/components/ArticleList/index.tsx
@@ -4,16 +4,21 @@ import styles from './index.module.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { getArticleList } from '@store/actions/home'
 import { RootStateType } from '@/types/store'
+import { InfiniteScroll } from 'antd-mobile'
 
 type PropsType = {
   channelId: number
 }
+
 const ArticleList = ({ channelId }: PropsType) => {
   const dispatch = useDispatch()
   const { channelArticles } = useSelector((state: RootStateType) => state.home)
-  useEffect(() => {
-    dispatch(getArticleList(channelId, Date.now()))
-  }, [])
+
+  const hasMore = true
+  const loadMore = async () => {
+    await dispatch(getArticleList(channelId, channelArticles[channelId]?.timestamp || Date.now()))
+  }
+
   return (
     <div className={styles.root}>
       {/* 文章列表中的每一项 */}
@@ -25,9 +30,22 @@ const ArticleList = ({ channelId }: PropsType) => {
             </div>
           ))
       }
-
+      {/* 上拉加载更多（antd-mobile无限滚动） */}
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} />
     </div>
   )
 }
 
 export default ArticleList
+
+// 01、antd-mobile中的InfiniteScroll组件可以实现上拉加载更多，只需将该组件放置于列表下方，当组件出现在可视区时就会加载一次（基于此，可以替代useEffect中依赖项为[]时的初次发请求）
+// 02、InfiniteScroll组件的loadMore属性必须返回一个promise，而axios请求返回的就是promise，因此通常会在该属性的函数体内进行异步请求
+// 02、InfiniteScroll 会自动对 loadMore 函数加锁，避免重复的请求，但是前提是 loadMore 函数需要返回一个正确的 Promise
+// 02、例如：const loadMore = async ()=>{ await axios() } 或者 const loadMore =()=>{ return axios() }
+// 03、InfiniteScroll组件的hasMore属性是一个布尔值，该布尔值的结果取决于上拉加载更多的列表是否还有数据（因为此处接口有问题，因此人为设定为当加载完100条后就设置为false）
+// 03、当前项目接口返回的timestamp时间戳，就是判断列表是否还有数据的依据，当timestamp为null时，则数据加载完毕，因此 const hasMore = timestamp !== null
+// 03、但是该接口并未完善导致时间戳的频道不准确，因此只能人为的设限当加载100条后置为false不再做无限加载，即 const hasMore =  article.length <= 100
+// 04、getArticleList这个actionCreator需要传入频道id以及时间戳
+// 04、A 如果是传入服务器返回的时间戳channelArticles[channelId]?.timestamp，则是请求历史推荐数据
+// 04、B 如果是传入当前时间戳Date.now()，则是请求当前最新的推荐数据
+// 05、因为无限加载做的是文章数据的拼接，因此需要修改action.type === 'home/getChannelArticleList'时的reducer,将请求回来的数据的逻辑由直接赋值改成新旧数据的展开与拼接
--- src/store/reducers/home/index.ts
+++ src/store/reducers/home/index.ts
@@ -40,12 +40,13 @@ const home = (state = initState, action: HomeActionType): HomeStateType => {
   }
   if (action.type === 'home/getChannelArticleList') {
     const { channelId, timestamp, articleList } = action.payload
+    const oldArticleList = state.channelArticles[channelId]?.articleList ?? []
     return {
       ...state,
       channelArticles: {
         ...state.channelArticles,
         [channelId]: {
-          articleList,
+          articleList: [...oldArticleList, ...articleList],
           timestamp: +timestamp,
         }
       }
@@ -56,21 +57,35 @@ const home = (state = initState, action: HomeActionType): HomeStateType => {
 
 export default home
 
-// 03、不同频道对应渲染不同的文章列表，因此设置action时，需要将频道id也加进来，与服务器返回的字段进行结合
-// 03、而不再是像其他的action那样，只需要指定一个必须的type属性，再额外配置一个自定义属性来全盘承接服务器返回的数据
-// 03、此处自定义配置了自定义属性对象，在对象内再次配置自定义channelId、timestamp、articlesList来分别承接频道、服务器返回的时间戳、服务器返回的频道对应文章列表
-// 之前：{type: 'xxx', payload: res.xxx.xxx }
-// 此时：{type: 'xxx', payload: { channelId: 频道id， timestamp:res.xxx.xxx, articlesList:res.xxx.xxxx } }
-// 后续，定义state时：channelArticles:{[channelId]:{timestap, articleList},[channelId]:{timestap, articleList},... }
-// 以上payload是自定义的属性，channelId、timestamp、 articlesList都是自定义的字段，后续只需要将频道id与服务器返回的数据赋值给这些字段变量即可
-// 之所以，这么做是为了后续通过不同的频道id来渲染不同的文章列表，否则后续的逻辑会很难处理
-// 另外，这些必须在定义数据的类型的时候就应该考虑好（可以先调用一次接口，才能明确服务器返回的数据是什么类型）
+// 01、不同频道对应渲染不同的文章列表，因此设置action时，需要将频道id也加进来，与服务器返回的字段进行结合
+// 01、而不再是像其他的action那样，只需要指定一个必须的type属性，再额外配置一个自定义属性来全盘承接服务器返回的数据
+// 02、此处自定义配置了自定义属性对象，在对象内再次配置自定义channelId、timestamp、articlesList来分别承接频道、服务器返回的时间戳、服务器返回的频道对应文章列表
+// 02、之前：{type: 'xxx', payload: res.xxx.xxx }
+// 02、此时：{type: 'xxx', payload: { channelId: 频道id， timestamp:res.xxx.xxx, articlesList:res.xxx.xxxx } }
+// 02、后续，定义state时：channelArticles:{[channelId]:{timestap, articleList},[channelId]:{timestap, articleList},... }
+// N1、以上payload是自定义的属性，channelId、timestamp、 articlesList都是自定义的字段，后续只需要将频道id与服务器返回的数据赋值给这些字段变量即可
+// N1、之所以，这么做是为了后续通过不同的频道id来渲染不同的文章列表，否则后续的逻辑会很难处理
+// N1、另外，这些必须在定义数据的类型的时候就应该考虑好（可以先调用一次接口，才能明确服务器返回的数据是什么类型）
 
-// 设置redux状态时，也要考虑清除，不同频道对应不同文章列表的状况
-// 格式：channelArticles: { 频道1：频道1对应返回的文章数据, 频道2：频道2对应返回的文章数据}
-// 例如：channelArticles: { 0: {timestamp: 1234, articles: [...]}, 1: {timestamp: 2345, articles:[...]},... }
+// 03、设置redux状态时，也要考虑清除，不同频道对应不同文章列表的状况
+// 03、格式：channelArticles: { 频道1：频道1对应返回的文章数据, 频道2：频道2对应返回的文章数据}
+// 03、例如：channelArticles: { 0: {timestamp: 1234, articles: [...]}, 1: {timestamp: 2345, articles:[...]},... }
 
-// N1、action.type === 'home/getChannelArticleList'，其中channelArticles是存储了全部频道文章的对象
-// N1、在切换频道时，只是渲染对应频道的文章，因此需要...state.channelArticles展开，然后渲染当前切换到的频道的文章列表
-// N1、即，[channelId]: { articles, timestamp: +timestamp }，其中因为类型不一致的问题，因此需要使用'+'进行隐式转换（将string类型转成number类型）
+// 04、action.type === 'home/getChannelArticleList'，其中channelArticles是存储了全部频道文章的对象
+// 04、在切换频道时，只是渲染对应频道的文章，因此需要...state.channelArticles展开，然后渲染当前切换到的频道的文章列表
+// 04、即，[channelId]: { articles, timestamp: +timestamp }，其中因为类型不一致的问题，因此需要使用'+'进行隐式转换（将string类型转成number类型）
 
+// 05、因为无限加载做的是文章数据的拼接，因此需要修改action.type === 'home/getChannelArticleList'时的reducer,将请求回来的数据的逻辑由直接赋值改成新旧数据的展开与拼接
+
+// 06、获取之前的 某个频道对应的文章列表 有两种方式
+// 06、方式1： `const oldArticleList = state.channelArticles[channelId]?.articleList ?? []`
+// 06、方式2：`const oldArticleList = state.channelArticles[channelId]?.articleList || []`
+// 06、第一个单`?`可选链操作符是为了防止一开始频道数据为空时往后取articleList的值导致报错
+// 06、第二个双`?`它只会判断undefined与null的情况，即如果articleList是undefined或者null时，才会往后取[]，避免undefined或者null使用展开运算符报错
+
+// 运算符 ? 、?? 、!、||、&&的区别 
+// 01、? 是可选链操作符，避免undefined往后取值报错
+// 02、?? 只会对undefined或null进行短路，在短路运算中，前者如果是undefined或者null时，才会往后取值
+// 03、! 是TS中的非空断言，表示数据不为空
+// 04、|| 是短路运算中的逻辑或，在短路运算中，前者的结果为false时，会往后取值（作为判断条件时，任意一个条件成立即可）
+// 05、&& 是短路运算中的逻辑与，在短路运算中，前者的结果为true时，会往后取值（作为判断条件时，两个条件必须同时成立）
