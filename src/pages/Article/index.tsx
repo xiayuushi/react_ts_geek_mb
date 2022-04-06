0a96fdebeb5e91a5a965ab844024fdda85e20b2f
xiayuushi <xiayuushi@qq.com>
2022-04-06 14:38:15 +0800

    文章详情 - 使用dompurify防止xss攻击

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 import styles from './index.module.scss'
-import { getArticleDetail } from '@/store/actions/article';
-import { useParams } from 'react-router-dom';
+import { getArticleDetail } from '@/store/actions/article'
+import { useParams } from 'react-router-dom'
 import dayjs from 'dayjs'
 import classNames from 'classnames'
-import useInitState from '@/hooks/useInitState';
+import useInitState from '@/hooks/useInitState'
+import DOMPurify from 'dompurify'
 
 const Article = () => {
   const params = useParams<{ id: string }>()
@@ -40,7 +41,7 @@ const Article = () => {
           <div className="content">
             <div
               className="content-html dg-html"
-              dangerouslySetInnerHTML={{ __html: articleDetail.content }}
+              dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(articleDetail.content) }}
             />
             <div className="date">
               发布文章时间：{dayjs(articleDetail.pubdate).format('YYYY-MM-DD')}
@@ -52,4 +53,17 @@ const Article = () => {
   );
 }
 
-export default Article;
+export default Article
+
+// 01、在react项目中解析渲染富文本必须使用到DOM标签的dangerouslySetInnerHTML属性
+// 02、在react项目中应该谨慎使用dangerouslySetInnerHTML属性（尤其是让用户输入的内容一定要确保内容安全），因为易遭受xss跨站脚本攻击
+// 03、xss攻击通常会与csrf（跨站伪造）攻击配合获取用户数据
+// 04、dompurify是一个可以清除可能潜在风险内容的库，专门用于防范xss攻击
+// 05、dompurify可以在不同的框架中使用，不局限于react或者vue，也可以在原生JS中使用
+
+// dompurify使用流程
+// st1、安装dompurify及类型声明文件：yarn add dompurify @types/dompurify
+// st2、在需要使用的组件中导入并使用：import DOMPurify from 'dompurify'
+// st3、调用该插件的sanitize(dirty)清除可能潜在风险的输入内容：DOMPurify.sanitize(此处传入可能存在潜在风险的内容如富文本)
+// st3、例如：在dangerouslySetInnerHTML属性的__html值的地方使用：<div dangerouslySetInnerHTML={{_html: DOMPurify.sanitize(富文本内容)}}></div>
+
